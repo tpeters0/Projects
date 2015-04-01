0285a7826169c98e0e2ab0d78ac8f436001f1421
Tammy Peters <tamarapeters2@gmail.com>
2015-03-31 20:44:18 -0400

    initial commit

---
+++ Project_1/playground.rb
@@ -0,0 +1,9 @@
+#experiment
+row_A = [" ~ ", " ~ "," ~ "," ~ "," ~ "," ~ "," ~ "," ~ "," ~ "," ~ "]
+
+puts row_A.join("|")
+# grid_hash = {row_A_array => []}
+#
+# puts("")
+# puts(grid_hash[row_A_array].join("|"))
+# puts("")
