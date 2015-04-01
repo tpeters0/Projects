54f43c47c8c9ae226588d4b89f701b6eeacec7a5
Tammy Peters <tamarapeters2@gmail.com>
2015-03-31 23:58:13 -0400

    second commit

--- Project_1/playground.rb
+++ Project_1/playground.rb
@@ -1,9 +1,27 @@
 #experiment
-row_A = [" ~ ", " ~ "," ~ "," ~ "," ~ "," ~ "," ~ "," ~ "," ~ "," ~ "]
+column_lables = ["   ", " 1 ", " 2 ", " 3 ", " 4 "," 5 "," 6 "," 7 "," 8 "," 9 ", " 10", ""]
+row_A =         [" A ", " ~ ", " ~ ", " ~ ", " ~ "," ~ "," ~ "," ~ "," ~ "," ~ ", " ~ ", ""]
+row_B =         [" B ", " ~ ", " ~ ", " ~ ", " ~ "," ~ "," ~ "," ~ "," ~ "," ~ ", " ~ ", ""]
+row_C =         [" C ", " ~ ", " ~ ", " ~ ", " ~ "," ~ "," ~ "," ~ "," ~ "," ~ ", " ~ ", ""]
+# row_D = []
+# row_E = []
+# row_F = []
+# row_G = []
+# row_H = []
+# row_I = []
+# row_J = []
 
-puts row_A.join("|")
-# grid_hash = {row_A_array => []}
-#
-# puts("")
-# puts(grid_hash[row_A_array].join("|"))
-# puts("")
+ocean_hash = {row_A => row_A,
+row_B => row_B,
+row_C => row_C
+}
+
+puts ""
+puts column_lables.join("|")
+puts("--" * 22)
+puts ocean_hash[row_A].join("|").to_s
+puts("--" * 22)
+puts ocean_hash[row_B].join("|").to_s
+puts("--" * 22)
+puts ocean_hash[row_C].join("|").to_s
+puts("")
---
+++ Project_1/preproduction/README.md
@@ -0,0 +1,9 @@
+Title: Sink, Sank, Sunk
+
+Variation of a 'Battleship' style game where user uses the command line interface to send missiles to certain ocean grid coordinates hoping to hit an enemy ship.
+
+There are two hidden ships, both 5 segments (rows or columns) long.  When all 5 segments of a ship have been 'hit,' the ship is sunk.  When both ships are sunken, the player wins.
+
+This app is currently in development.
+
+March 31, 2015
